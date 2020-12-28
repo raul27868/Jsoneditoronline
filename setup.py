8cd5e6299a478cabf40c531af57f43beb3b1c051
Raul Reyero <34958262+raul27868@users.noreply.github.com>
2020-12-28 22:40:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email='raul.reyero.diez@gmail.com',
     description='Multiples utilities',
     packages=find_packages(),    
-    install_requires=[   ],
+    install_requires=[ 'simplejson'  ],
 )
  
 
