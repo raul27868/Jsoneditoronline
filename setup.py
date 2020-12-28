4ba63b1ca4fc840619489ec71258ba4cf398baac
Raul Reyero <34958262+raul27868@users.noreply.github.com>
2020-12-28 20:54:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,7 @@ import datetime
 import requests
 import json
 
-setup(
-    name='Jsoneditoronline',
-    version='1.0.0',
-    url='https://github.com/raul27868/Jsoneditoronline.git',
-    author='Raul Reyero',
-    author_email='raul.reyero.diez@gmail.com',
-    description='Api for https://jsoneditoronline.org',
-    packages=find_packages(),    
-    install_requires=['requests', 'datetime', 'json' ],
-)
+
  
 
 class Jsoneditoronline:
